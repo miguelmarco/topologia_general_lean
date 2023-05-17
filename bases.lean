4444c49339c0f9255631b9924193adf676040b87
Miguel Marco <mmarco@unizar.es>
2023-05-17 18:25:11 +0200

    Limpiar bases

--- bases.lean
+++ bases.lean
@@ -6,12 +6,23 @@ open set
 open topologicos
 open topologicos.espacio_topologico
 
+
+/-
+En esta sección, `X` va a ser un espacio topológico
+-/
 variables {X : Type} [espacio_topologico X]
 
-def base (B : set (set X)) := B ⊆ abiertos ∧ ∀ (U : set X), abierto U →  ∀ x ∈ U , ∃ V ∈ B, x ∈ V ∧ V ⊆ U 
+/-
+Definimos base como una familia de abiertos que genera toda la topología mediante uniones
+-/
+def base (B : set (set X)) := 
+B ⊆ abiertos ∧ 
+∀ (U : set X), abierto U →  ∀ x ∈ U , ∃ V ∈ B, x ∈ V ∧ V ⊆ U 
 
 
-lemma caracterizacion_base (B : set (set X)) : base B ↔ B ⊆ abiertos ∧ ∀ U : set X, abierto U → ∃ F ⊆ B, ⋃₀ F = U :=
+
+lemma caracterizacion_base (B : set (set X)) : 
+base B ↔ B ⊆ abiertos ∧ ∀ U : set X, abierto U → ∃ F ⊆ B, ⋃₀ F = U :=
 begin
   split,
   {
@@ -47,7 +58,6 @@ begin
         specialize hBcubre x hx,
         rcases hBcubre with ⟨V,hVB,hxV,hVU⟩ ,
         use V,
-
         split,
         {
           split,
@@ -62,7 +72,6 @@ begin
           exact hxV,
         },
       },
-      
     },
   },
   {
@@ -95,8 +104,8 @@ begin
         intros y hy,
         use V,
         split,
-        assumption,
-        assumption,
+        exact hVF,
+        exact hy,
       }
     }
   }
@@ -188,392 +197,32 @@ end
 def intersecciones_finitas {X : Type} (F : set (set X)) : (set (set X)) :=
 {U : set X | ∃ (S : set (set X)), set.finite S ∧ S ⊆ F ∧ ⋂₀ S = U }
 
-def abiertos_generados {X : Type} ( F : set (set X)) := {U : set X | ∃ FU : set (set X), FU ⊆ intersecciones_finitas F ∧ ⋃₀ FU = U}
-
-inductive generados {X : Type} (F : set (set X)) : set X → Prop 
-| elemento  (U : set X) (h : U ∈ F) :  generados  U
-| interseccion_finita (S : set (set X)) (hS : ∀ U ∈ S,  generados U) (hF : set.finite S): generados ⋂₀ S
-| union_familia (S : set (set X)) (hS : ∀ U ∈ S, generados U ) : generados ⋃₀ S
-
-
-lemma generados_eq_abiertos_generados {X : Type} (F : set (set X)) : abiertos_generados F = generados F :=
+lemma interseccion_intersecciones_finitas {X : Type} (F : set (set X)) (A B : set X) (ha : A ∈ intersecciones_finitas F) (hb : B ∈ intersecciones_finitas F) :
+A ∩ B ∈ intersecciones_finitas F :=
 begin
-  ext U,
-  split,
+  cases ha with Fa hFa,
+  cases hb with Fb hFb,
+  use Fa ∪ Fb,
   {
-    intro h,
-    cases h with S hS,
-    cases hS with hSinf hSU,
-    rw ← hSU,
-    apply generados.union_familia,
-    intros V hV,
-    specialize hSinf hV,
-    cases hSinf with B hB,
-    cases hB with hBfin hBV,
-    cases hBV with hBV hBin,
-    rw ← hBin,
-    apply generados.interseccion_finita,
+    split,
     {
-      intros U hU,
-      apply generados.elemento,
-      apply hBV,
-      exact hU,
+      simp only [finite_union],
+      exact ⟨ hFa.1,hFb.1⟩,
+    },
+    split,
+    {
+      simp only [union_subset_iff],
+      exact ⟨ hFa.2.1,hFb.2.1⟩,
     },
     {
-      exact hBfin,
+      rw ← hFa.2.2,
+      rw ← hFb.2.2,
+      rw sInter_union,
     },
   },
-  {
-    intro h,
-    induction h with S hS1  S hS1 hS2 hS3 S hS4 hS5 hS6,
-     {
-      use {S},
-      simp only [and_true, eq_self_iff_true, set.singleton_subset_iff, set.sUnion_singleton],
-      use {S},
-      simp only [true_and,  set.sInter_singleton,  and_true,  eq_self_iff_true,  set.finite_singleton, set.singleton_subset_iff, hS1],
-     },
-     {
-      revert hS1 hS3,
-      apply set.finite.induction_on hS2,
-      {
-        intros,
-        simp only [set.sInter_empty],
-        use {univ},
-        simp only [and_true, eq_self_iff_true, set.singleton_subset_iff, set.sUnion_singleton],
-        use ∅,
-        simp only [set.empty_subset, set.sInter_empty, eq_self_iff_true, set.finite_empty, and_self],
-      },
-      {
-        intros a s has hsfin h1 h2 h3,
-        simp only [mem_insert_iff, forall_eq_or_imp] at h2,
-        cases h2 with h2a h2s,
-        specialize h1 h2s,
-        simp only [mem_insert_iff, forall_eq_or_imp] at h3,
-        cases h3 with h3a h3s,
-        specialize h1 h3s,
-        cases h1 with Fs hFs,
-        cases hFs with hFsfin hFsinter,
-        cases h3a with Fa hFa,
-        cases hFa with hFafin hFainter,
-        use {V : set X | ∃ W1 ∈ Fa, ∃ W2 ∈ Fs, V = W1 ∩ W2},
-        split,
-        {
-          intros V hV,
-          rcases hV with ⟨ W1,⟨hW1, ⟨  W2,⟨ hW2 ,hV⟩ ⟩ ⟩ ⟩,
-          specialize hFafin hW1,
-          specialize hFsfin hW2,
-          rcases hFafin with ⟨ R1 ,⟨ hR1fin,⟨ hR1F,hR1W1⟩ ⟩⟩ ,
-          rcases hFsfin with ⟨ R2 ,⟨ hR2fin,⟨ hR2F,hR2W2⟩ ⟩⟩ ,
-          use R1 ∪ R2,
-          split,
-          {
-            rw finite_union,
-            exact ⟨ hR1fin,hR2fin⟩,
-          },
-          split,
-          {
-            exact union_subset hR1F hR2F,
-          },
-          {
-            rw hV,
-            rw sInter_union,
-            rw hR1W1,
-            rw hR2W2,
-          },
-        },
-        {
-          ext x,
-          split,
-          {
-            intro hx,
-            cases hx with V hV,
-            cases hV with hV hxV,
-            rcases hV with ⟨ W1,⟨hW1,⟨W2,⟨hW2,hV⟩⟩⟩⟩,
-            simp only [sInter_insert, mem_inter_iff, mem_sInter],
-            split,
-            {
-              rw ← hFainter,
-              use W1,
-              split,
-              {
-                exact hW1,
-              } ,
-              {
-                rw hV at hxV,
-                exact hxV.1,
-              },
-            },
-            {
-
-              intros t ht,
-              rw hV at hxV,
-              have haux : x ∈ ⋃₀ Fs,
-              {
-                use W2,
-                exact ⟨ hW2,hxV.2⟩,
-              },
-              rw hFsinter at haux,
-              exact haux t ht,
-            },
-          },
-          {
-            intro hx,
-            simp only [sInter_insert, mem_sInter] at hx,
-            cases hx with hxa hxs,
-            rw  ← hFainter at hxa,
-            rw  ← hFsinter at hxs,
-            rcases hxa with ⟨W1,⟨hW1,hW1x⟩ ⟩ ,
-            rcases hxs with ⟨W2,⟨hW2,hW2x⟩ ⟩ ,
-            use W1 ∩ W2,
-            split,
-            {
-              use W1,
-              split, exact hW1,
-              use W2,
-              split, exact hW2,
-              refl,
-            },
-            exact ⟨hW1x,hW2x⟩,
-          },
-        }
-      }
-     },
-     {
-      unfold abiertos_generados at *,
-      have hS5' : ∀ (U : S), ∃ (FU : set (set X)), FU ⊆ intersecciones_finitas F ∧ ⋃₀ FU = ↑U,
-      {
-        rintro ⟨U,hU⟩,
-        specialize hS5 U hU,
-        exact hS5,
-      },
-      choose g Fu hFU using hS5',
-      use ⋃₀ (range g),
-      split,
-      {
-        intros V hV,
-        cases hV with W hW,
-        cases hW with hW hVW,
-        cases hW with Z hZ,
-        specialize Fu Z,
-        apply Fu,
-        rw hZ,
-        exact hVW,
-      },
-      {
-        ext x,
-        split,
-        {
-          intro hx,
-          cases hx with V hV,
-          cases hV with hV hxV,
-          cases hV with W hW,
-          cases hW with hW hVW,
-          cases hW with Z hZ,
-          specialize hFU Z,
-          rw hZ at hFU,
-          use ⋃₀ W,
-          split,
-          {
-            rw hFU,
-            exact Z.2,
-          },
-          {
-            use V,
-            exact ⟨ hVW,hxV⟩,
-          },
-        },
-        {
-          intro hx,
-          cases hx with V hV,
-          cases hV with hVS hxV,
-          have hFU2 := hFU ⟨ V,hVS⟩,
-          dsimp  at hFU2,
-          rw ← hFU2 at hxV,
-          cases hxV with W hW,
-          cases hW with hWgV hxW,
-          use W,
-          split,
-          {
-            use g ⟨V,hVS⟩,
-            simp only [mem_range_self, true_and,hWgV],
-          },
-          {
-            exact hxW,
-          }
-        },
-      }
-     },
-      
-
-
-    /- apply generados.rec,
-    {
-      intros U hU,
-      use {U},
-      simp only [intersecciones_finitas, singleton_subset_iff, mem_set_of_eq, sUnion_singleton, eq_self_iff_true, and_true],
-      use {U},
-      simp only [finite_singleton, singleton_subset_iff, sInter_singleton, eq_self_iff_true, and_true, true_and],
-      exact hU,
-    },
-    {
-      intros S hS hSfin,
-      revert hS,
-      apply set.finite.induction_on hSfin,
-      {
-        simp only [mem_empty_iff_false, forall_const, sInter_empty],
-        use { univ},
-        simp only [singleton_subset_iff, sUnion_singleton, eq_self_iff_true, and_true],
-        use ∅,
-        simp only [finite_empty, empty_subset, sInter_empty, eq_self_iff_true, and_self],
-      },
-      {
-        intros U s hUnS hsfin h1 h2 h3,
-        unfold abiertos_generados at *,
-        simp only [sInter_insert, mem_insert_iff, forall_eq_or_imp] at *,
-        specialize h1 h2.2 h3.2,
-        cases h1 with FU hFU,
-        cases hFU with hFU1 hFU2,
-        cases h3.1 with A hA,
-        let mF := {V : set X | ∃ V1 ∈ FU, ∃ V2 ∈ A, V = V1 ∩ V2},
-        use mF,
-        split,
-        {
-          intros V hV,
-          cases hV with V1 hV1,
-          cases hV1 with hV1 hV,
-          cases hV with V2 hV2,
-          cases hV2 with hV2 hV,
-          cases hA with hA1 hA2,
-          specialize hA1 hV2,
-          cases hA1 with FA2 hFA2,
-          specialize hFU1 hV1,
-          cases hFU1 with FA1 hFA1,
-          use FA1 ∪ FA2,
-          split,
-          {
-            rw finite_union,
-            tidy,
-          },
-          split,
-          {
-            apply union_subset hFA1.2.1 hFA2.2.1,
-          },
-          {
-            rw sInter_union,
-            rw hV,
-            rw hFA1.2.2,
-            rw hFA2.2.2,
-          },
-        },
-        {
-          ext x,
-          split,
-          {
-            intro hx,
-            cases hx with V hV,
-            cases hV with hVV hVV2,
-            r,cases hVV with ⟨V1,⟨hV1,⟨V2,⟨hV2,hV⟩⟩⟩⟩,
-            rw hV at hVV2,
-            cases hVV2 with hxV2 hxV2,
-            split,
-            {
-              rw ← hA.2,
-              use V2, finish, 
-            },
-            {
-              rw ← hFU2,
-              use V1, finish,
-            },
-          },
-          {
-            intro hx,
-            cases hx with hx1 hx2,
-            cases hA with hA1 hA2,
-            rw ← hFU2 at hx2,
-            rw ← hA2 at hx1,
-            cases hx1 with x1 hx1,
-            cases hx1 with hx1 hx1F,
-            cases hx2 with x2 hx2,
-            cases hx2 with hx2 hx2F,
-            use x1 ∩ x2,
-            split,
-            {
-              use x2,
-              split,
-              use hx2,
-              use x1,
-              split,
-              use hx1,
-              rw inter_comm,
-            },
-            {
-              tidy,
-            },
-          },
-        }
-      }
-    },
-    {
-      intros S hSgen hSgenab,
-      unfold abiertos_generados at *,
-      let g := {V ∈ intersecciones_finitas F | ∃ W ∈ S, V ⊆ W },
-      use g,
-      split,
-      {
-        intros x hx,
-        cases hx with hx1 hx2,
-        exact hx1,
-      },
-      {
-        ext x,
-        split,
-        {
-          intro hx,
-          cases hx with V hV,
-          cases hV with hV hxV,
-          cases hV with hVF hW,
-          cases hW with W hW,
-          use W,
-          cases hW,
-          split, assumption,
-          apply hW_h, assumption,
-
-        },
-        {
-          intro hx,
-          cases hx with V hV,
-          cases hV with hVS hxV,
-          specialize hSgenab V hVS,
-          cases hSgenab with FU hFU,
-          cases hFU with hFUin hFUV,
-          rw ←  hFUV at hxV,
-          cases hxV with W hW,
-          cases hW with hWFU hxFU,
-          use W,
-          split,
-          {
-            split,
-            {
-              apply hFUin,
-              exact hWFU,
-            },
-            {
-              use V,
-              split, exact hVS,
-              rw ← hFUV,
-              intros x hx,
-              use W,
-              split,
-              assumption, assumption,
-            },
-          },
-          exact hxFU,
-        },
-      },
-    },
-    exact h, -/
-  }
 end
+
+def abiertos_generados {X : Type} ( F : set (set X)) := {U : set X | ∃ FU : set (set X), FU ⊆ intersecciones_finitas F ∧ ⋃₀ FU = U}
 
 
 
@@ -595,203 +244,136 @@ instance topologia_generada {X : Type} (F : set (set X)) (h1 : B1 F) (h2 : B2 F)
   end,
   abierto_union := begin
     intros F1 hF1,
-    unfold abiertos_generados at hF1,
-    choose g hg using hF1,
-    let f : ↥F1 → set (set X),
-    {
-      intro U,
-      apply g,
-      apply U.2,
-    },
-    have hf : ∀ x : ↥F1, f x ⊆ intersecciones_finitas F ∧ ⋃₀ f x  = x,
-    {
-      intro x,
-      apply hg,
-    },
-    let UF1 := ⋃₀ (range f),
-    use UF1,
+    use {U ∈ intersecciones_finitas F | U ⊆ ⋃₀ F1},
     split,
     {
-      intros U hU,
-      cases hU with V hV,
-      cases hV with hfV hV,
-      cases hfV with A hA,
-      specialize hf A,
-      cases hf with hf1 hf2,
-      apply hf1,
-      rw   hA,
-      exact hV,
+      simp only [sep_subset],
     },
     {
       ext x,
       split,
       {
         intro hx,
-        cases hx with hU hU,
-        cases hU with hfU hxU,
-        cases hfU with V hV,
-        cases hV with hfV hUV,
-        cases hfV with B hB,
-        specialize hf B,
-        cases hf with hf1 hf2,
-        rw hB at hf2,
-        use B,
-        split, apply B.2,
-        rw ← hf2,
-        use hU,
-        split,
-        assumption,
-        assumption,
+        rcases hx with ⟨ U,⟨ ⟨ hUint, hUF⟩ ,hxu⟩⟩ ,
+        apply hUF hxu,
       },
       {
         intro hx,
-        cases hx with U hU,
-        cases hU with hU hxU,
-        have hfU := hf ⟨U,hU⟩,
-        cases hfU with elim hfU,
-        clear elim,
-        have hxUU : x ∈ ⋃₀ (f ⟨U,hU⟩),
-        {
-          rw hfU,
-          exact hxU,
-        },
-        cases hxUU with V hV,
-        cases hV with hV hxV,
+        rcases hx with ⟨ U ,⟨ hUF1,hxU⟩ ⟩ ,
+        specialize hF1 hUF1,
+        rcases hF1 with ⟨FU ,⟨ hFUinf,hFU⟩ ⟩ ,
+        rw ← hFU at hxU,
+        rcases hxU with ⟨ V, ⟨ hVF1,hxV⟩⟩ ,
         use V,
         split,
         {
-          use f ⟨ U,hU⟩,
-          simp only [set.mem_range_self, and_self, hV],
+          split,
+          {
+            exact hFUinf hVF1,
+          },
+          {
+            dsimp,
+            intros y hyV,
+            use U,
+            split,
+            {
+              exact hUF1,
+            },
+            {
+              rw ← hFU,
+              use V,
+              exact ⟨ hVF1,hyV⟩,
+            },
+          },
         },
         {
           exact hxV,
-        }
-      }
-    }
+        },
+      },
+    },
   end,
   abierto_interseccion := begin
     intros S hS hSfin,
     revert hS,
-    revert hSfin,
-    revert S,
-    have puniv : univ ∈ abiertos_generados F,
+    apply set.finite.induction_on hSfin,
     {
-      use F,
-      split,
-      {
-        intros U hU,
-        use {U},
-       simp only [hU,  set.sInter_singleton, eq_self_iff_true, set.finite_singleton, and_self, set.singleton_subset_iff],  
-      },
-      exact h1,
+      simp only [empty_subset, sInter_empty, forall_true_left],
+      use {univ},
+      simp only [singleton_subset_iff, sUnion_singleton, eq_self_iff_true, and_true],
+      use ∅ ,
+      simp only [finite_empty, empty_subset, sInter_empty, eq_self_iff_true, and_self],
     },
-    have pif := propiedad_interseccion_finita ( X) (abiertos_generados F) puniv, 
-    cases pif with pif1 pif2,
-    apply pif1,
-    clear pif2 pif1,
-    intros U V hU hV,
-    cases hU with fU hfU,
-    cases hV with fV hfV,
-    use { A : set X | ∃ u ∈ fU, ∃ v ∈ fV, A = u ∩ v},
-    split,
     {
-      intros A hA,
-      rcases hA with ⟨u,hU,v,hV,hA⟩,
-      have hfU1 := hfU.1 hU,
-      have hfV1 := hfV.1 hV,
-      rw hA,
-      cases hfU1 with AU hAU,
-      cases hfV1 with AV hAV,
-      use AU ∪ AV,
+      intros a s hnins hsfin hinds h,
+      have hagen : a ∈ abiertos_generados F,
+      {
+        apply h,
+        left,
+        refl,
+      },
+      have hsgen : s ⊆ abiertos_generados F,
+      {
+        intros x hx,
+        apply h,
+        right,
+        exact hx,
+      },
+      specialize hinds hsgen,
+      rcases hagen with ⟨ Fa ,⟨ hFafin, hFa⟩ ⟩ ,
+      rcases hinds with ⟨ fS, ⟨ hFsfin, hFs⟩⟩,
+      use {U ∈ intersecciones_finitas F | U ⊆ a ∩ (⋂₀ s)},
       split,
       {
-        rw finite_union,
-        split,
-          exact hAU.1,
-          exact hAV.1,
+        rintros U ⟨hU,hU2⟩ ,
+        exact hU,
       },
       {
+        ext x,
         split,
         {
-          apply union_subset,
-            exact hAU.2.1,
-            exact hAV.2.1,
+          intro hx,
+          rcases hx with ⟨ U ,⟨ hUF,hUas⟩ ,hxU⟩,
+          simp only [sInter_insert],
+          exact hUas hxU,
         },
         {
-          rw ← hAU.2.2,
-          rw ← hAV.2.2,
-          ext,
+          intro hx,
+          simp only [sInter_insert] at hx,
+          cases hx with hxa hxs,
+          induction hFa,
+          induction hFs,
+          rcases hxa with ⟨ Ua , ⟨ hUaF, hxUa⟩⟩ ,
+          rcases hxs with ⟨ Us , ⟨ hUsF, hxUs⟩⟩ ,
+          use Ua ∩ Us,
           split,
           {
-            intro hx,
             split,
             {
-              intros B hB,
-              apply hx,
-              left,
-              exact hB,
+              specialize hFafin hUaF,
+              specialize hFsfin hUsF,
+              apply interseccion_intersecciones_finitas ,
+              exact hFafin,
+              exact hFsfin,
             },
             {
-              intros B hB,
-              apply hx,
-              right,
-              exact hB,
+              simp only [subset_inter_iff],
+              split,
+              {
+                intros x hx,
+                use Ua,
+                exact ⟨hUaF,hx.1 ⟩,
+              },
+              {
+                intros x hx,
+                use Us,
+                exact ⟨hUsF,hx.2⟩,
+              },
             },
           },
           {
-            intro hx,
-            cases hx with hx1 hx2,
-            intros B hB,
-            cases hB,
-            {
-              apply hx1,
-              exact hB,
-            },
-            {
-              apply hx2,
-              exact hB,
-            },
+            exact ⟨hxUa,hxUs⟩,
           },
         },
-      },
-    },
-    {
-      ext,
-      split,
-      {
-        intro hx,
-        rcases hx with ⟨ A,⟨u,hu,v,hv,hA⟩ ,hxA⟩ ,
-        rw ← hfU.2,
-        rw ← hfV.2,
-        rw hA at hxA,
-        cases hxA with hxu hxv,
-        split,
-        {
-          use u,
-          tauto,
-        },
-        {
-          use v,
-          tauto,
-        },
-      },
-      {
-        intro hx,
-        cases hx with hxU hxV,
-        rw ← hfU.2 at hxU,
-        rw ← hfV.2 at hxV,
-        rcases hxU with ⟨ u, hu,hxu⟩,
-        rcases hxV with ⟨v,hv,hxv⟩,
-        use u ∩ v,
-        split,
-        {
-          use u,
-          use hu,
-          use v,
-          use hv,
-        },
-        split,
-        assumption, assumption,
       },
     },
   end }
