8c3a0771d6e86853acf80b3bbe17eaacaa53231a
Miguel Marco <mmarco@unizar.es>
2023-06-30 15:01:08 +0200

    añadir algunas simplifcaciones en funciones

--- funciones.lean
+++ funciones.lean
@@ -8,15 +8,71 @@ open function
 -- definimos la función identidad
 def identidad {X : Type} : X → X := λ x, x
 
+-- demostramos un lema trivial para que el simplificador lo use
+@[simp]
+lemma identidad_def {X : Type} (x : X) : identidad x = x :=
+begin
+  refl,
+end
+
+-- algunas propiedades de la aplicación identidad
+-- las marcamos como `[simp]`  para que el simplificador las use
+@[simp]
+lemma identidad_neutro_derecha {X Y: Type} (f : X → Y) : f ∘ identidad = f :=
+begin
+  refl,
+end
+
+@[simp]
+lemma identidad_neutro_izquierda {X Y : Type} (f : X → Y) : identidad ∘ f = f :=
+begin
+  refl,
+end
+
+
 -- y lo que significa que dos funciones sean inversas
 def inversas  {X Y : Type} (f : X → Y) (g : Y → X ) :=
      g ∘ f = identidad ∧ f ∘ g = identidad
 
 
+-- ser inversas es reflexiva
 lemma inversas_sii {X Y : Type } (f : X → Y) (g : Y → X ) : inversas f g ↔ inversas g f :=
 begin
   unfold inversas,
   itauto,
+end
+
+-- trivialmente, componer con la inversa es la identidad
+@[simp]
+lemma inversa_cancela_izquierda {X Y : Type} (f : X → Y) (g : Y → X) (h : inversas f g) :
+f ∘ g = identidad :=
+begin
+  cases h with hfg hgf,
+  exact hgf,
+end
+
+@[simp]
+lemma inversa_cancela_izquierda' {X Y : Type} (f : X → Y) (g : Y → X) (h : inversas g f) :
+f ∘ g = identidad :=
+begin
+  cases h with hfg hgf,
+  exact hfg,
+end
+
+@[simp]
+lemma inversa_cancela_derecha {X Y : Type} (f : X → Y) (g : Y → X) (h : inversas f g) :
+g ∘ f = identidad :=
+begin
+  cases h with hfg hgf,
+  exact hfg,
+end
+
+@[simp]
+lemma inversa_cancela_derecha' {X Y : Type} (f : X → Y) (g : Y → X) (h : inversas g f) :
+g ∘ f = identidad :=
+begin
+  cases h with hfg hgf,
+  exact hgf,
 end
 
 
@@ -24,50 +80,45 @@ end
 lemma inversa_unica {X Y : Type} (f : X → Y) (g h : Y → X) : inversas f g → inversas f h → g = h :=
 begin
   intros h1 h2,
-  unfold inversas at *,
-  cases h1 with h1a h1b,
-  cases h2 with h2a h2b,
-  ext,
-  have  haux1:  (f ∘ g) x = x,
-  {
-    unfold identidad at h1b,
-    rw h1b,
-  },
-  rw ← haux1,
-  change  ((g ∘ f) ∘ g) x = ((h ∘ f) ∘ g) x,
-  rw h1a,
-  rw h2a,
+  calc
+    g   =  g ∘ identidad : by {simp,}
+    ... =  g ∘ (f ∘ h)   :  by {simp [h2],}
+    ... =  (g ∘f ) ∘ h   :  by {simp,}
+    ... = identidad ∘ h  :  by {simp [h1],}  
+    ... =  h             : by {simp,}
 end
 
-
+-- una aplicación inyectiva y sobreyectiva tiene inversa
 lemma inversa_de_inyectiva_y_sobreyectiva {X Y : Type} (f : X →  Y) (hin : injective f) (hsob : surjective f ) :
 ∃ g : Y → X, inversas f g :=
 begin
-  choose g hg using hsob,
+  choose g hg using hsob, -- usamos la suprayectividad, y el axioma de elección, para definir una aplicación en sentido contrario
   use g,
   split,
   {
     ext,
     apply hin,
     simp,
-    specialize hg (f x),
     rw hg,
-    refl,
   },
-  ext,
-  specialize hg x,
-  exact hg,
+  {
+    ext,
+    simp,
+    apply hg,
+  }
 end
 
 
 def inversa {X Y : Type} (f : X → Y) (hin :bijective f) : Y → X := (equiv.of_bijective f hin).inv_fun
 
-lemma inversa_es_inversa_izquierda {X Y : Type} (f : X → Y) (hin :bijective f) : (inversa f hin ) ∘ f = identidad :=
+@[simp]
+lemma inversa_es_inversa_izquierda {X Y : Type} (f : X → Y) (hin : bijective f) : (inversa f hin ) ∘ f = identidad :=
 begin
   ext,
   apply (equiv.of_bijective f hin).left_inv,
 end
 
+@[simp]
 lemma inversa_es_inversa_derecha {X Y : Type} (f : X → Y) (hin :bijective f) : f ∘ (inversa f hin )  = identidad :=
 begin
   ext,
