ed30f30fe7ca98da29ccdb5d23f068fe74369911
Miguel Marco <mmarco@unizar.es>
2023-11-05 16:35:25 +0100

    añadidos ejercicios semana 6

--- ejercicios/semana6_resueltos.lean
+++ ejercicios/semana6_resueltos.lean
@@ -5,7 +5,7 @@ import tactic
 
 open topologicos topologicos.espacio_topologico function
 
-lemma ejer_2_6_8 (X Y : Type) [espacio_topologico X] [espacio_topologico Y] (f : X → Y) 
+lemma ejer_2_6_8_sol (X Y : Type) [espacio_topologico X] [espacio_topologico Y] (f : X → Y) 
     (hab : abierta f) (hsup : surjective f) (hcon : continua f) (𝓑 : set (set X)) (h𝓑 : base 𝓑) :
     base ({ (f '' B) | B ∈ 𝓑}) :=
   begin
